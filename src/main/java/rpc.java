d3e0d259b603d2ddcd65f76d66adf9c82700e217
christer Holm <ch@star78.se>
2021-09-27 11:34:53 +0200

    third r,s,p work now

--- src/main/java/rpc.java
+++ src/main/java/rpc.java
@@ -1,3 +1,4 @@
+import java.awt.*;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -9,7 +10,7 @@ public class rpc {
         Scanner scanner = new Scanner(System.in);
         String playerMove;
         while (true) {
-            System.out.println("Please Enter you Move (r, p, or s");
+            System.out.println("Please Enter you Move (r, p, or s)");
             playerMove = scanner.nextLine();
             if (playerMove.equals("r") || playerMove.equals("p") || playerMove.equals("s")) {
                 break;
@@ -17,6 +18,29 @@ public class rpc {
             System.out.println(playerMove + " is not valid movie");
         }
      System.out.println("Computer played:" + computerMove);
-
+if (playerMove.equals(computerMove)){
+    System.out.println("the game was a tie!");
+}
+else if (playerMove.equals("r")){
+ if (computerMove.equals("p")){
+     System.out.println("You lose!");
+ } else if (computerMove.equals("s")){
+     System.out.println("you win");
+ }
+}
+        else if (playerMove.equals("p")){
+            if (computerMove.equals("r")){
+                System.out.println("You win!");
+            } else if (computerMove.equals("s")){
+                System.out.println("you lose!");
+            }
+        }
+else {
+    if (computerMove.equals("p")){
+        System.out.println("You win!");
+    } else if (computerMove.equals("r")){
+        System.out.println("you lose!");
+    }
+}
     }
 }
