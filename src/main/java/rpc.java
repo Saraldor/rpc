5fd1c59bd9ed5c0d4ea626ea7c5e30a44872e143
christer Holm <ch@star78.se>
2021-09-27 11:03:55 +0200

    First line of code

---
+++ src/main/java/rpc.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class rpc {
+public static void main (String[]args){
+    String [] rps = {"r", "p", "s"};
+    String computerMove = rps[new Random().nextInt(rps.length)];
+Scanner scanner = new Scanner(System.in);
+System.out.println("Please Enter you Move");
+
+
+    }
+}
